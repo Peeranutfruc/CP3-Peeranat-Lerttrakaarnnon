d9ccc9968d5f0690dfff72d5ab715042a883b0ca
Peeranutfruc <106439054+Peeranutfruc@users.noreply.github.com>
2022-05-28 21:28:10 +0700

    Add files via upload

---
+++ Exercise 04_Peeranat_L.py
@@ -0,0 +1,9 @@
+FE = 80.5
+GB = 75
+ICS = 85
+CP = 90.5
+print("---Your Score---")
+print("Foundation English :",FE)
+print("General Business : ",GB)
+print("Introduction to CS :",ICS)
+print("Computer Programming :",CP)
